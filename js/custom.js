a20861bcb2a6e8e911147a8876dfad3fb2b6a6a1
Eddye <erios@sigcomt.com>
2018-09-05 12:13:09 -0500

    Asistencia

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+(function($) {
+    'use strict';
+
+    $(document).ready(function() {
+        $('ul.sf-menu').superfish({
+            animation: {
+                height: 'show'
+            }, // slide-down effect without fade-in
+            delay: 1200, // 1.2 second delay on mouseout
+            cssArrows: false
+        });
+
+        $("#sidebar, .mainWrapper").mCustomScrollbar({
+            theme: "minimal-dark"
+        });
+
+        var owl = $('.owl-carousel');
+        owl.owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 10,
+            autoplay: true,
+            autoplayTimeout: 5000
+        });
+
+        $("#sidebarCollapse").click(function() {
+            $('#sidebar, #content').toggleClass('active');
+
+            setTimeout(reloadSlider, 200);
+
+            var el = $("#sidebar");
+            var content = $("#content");
+
+            if (el.hasClass("mCS_disabled")) {
+                el.mCustomScrollbar("update");
+            } else {
+                el.mCustomScrollbar("disable", true);
+            }
+        });
+
+        $("#asistenciaDate").flatpickr({
+            enableTime: true,
+            dateFormat: "Y-m-d H:i",
+        });
+
+        $('ul#controlAlumnos > a').on('click', function(e) {
+            e.preventDefault();
+            $(this).tab('show');
+            console.log($(this));
+        })
+});
+
+    function reloadSlider() {
+        var owl = $('.owl-carousel');
+        owl.trigger('refresh.owl.carousel');
+    }
+
+
+})(jQuery);
---
+++ vendor/inputs/README.md
@@ -0,0 +1,6 @@
+Inputs
+======
+
+HTML form inputs with pure CSS and a little bit JS magic.
+
+[Documentation page](http://anjlab.github.io/inputs)
